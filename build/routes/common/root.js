9edf031db8a4d53b1a0df2ea2d44752f95095546
gogbajbobo <grigoriev.maxim@gmail.com>
2018-10-09 05:45:02 +0300

    build

--- build/routes/common/root.js
+++ build/routes/common/root.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const rootRoute = (router) => {
     router.route('/')
-        .get((req, res) => res.json({ info: 'hbud.server' }));
+        .get((req, res) => res.json({ info: 'gpi.poc.server' }));
 };
 exports.default = rootRoute;
